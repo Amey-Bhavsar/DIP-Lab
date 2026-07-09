ab32898dbb40d30046df5c2af6463ff2303254a0
Amey-Bhavsar <ameybhavsar797@gmail.com>
2026-07-09 17:38:15 +0530

    Adding the agorithms in UI

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from algorithms.spatial import mean_blur_filter , median_filter , gaussian_blur_filter , laplacian_filter
+from algorithms.edge_detection import sobel_edge_detection , prewitt_edge_detection
 
 st.title("DIP Lab")
 
@@ -9,8 +10,8 @@ uploaded_file = st.file_uploader("Upload an image" , type=["jpg" , "jpeg" , "png
 
 if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded Image")
-    selected_filter = st.selectbox("Choose a filter", ["Mean Blur", "Median Blur" , "Gaussian Blur" , "Sharpen (Laplacian)"])
-    if selected_filter != "Sharpen (Laplacian)" : 
+    selected_filter = st.selectbox("Choose a filter", ["Mean Blur", "Median Blur" , "Gaussian Blur" , "Sharpen (Laplacian)" , "Sobel Edge Detection" , "Prewitt Edge Detection"])
+    if selected_filter == "Mean Blur" or selected_filter == "Median Blur" or selected_filter == "Gaussian Blur":
         kernel_size = st.slider("Specify kernel size" , 3 , 15 , 3 , 2)
     if selected_filter == "Gaussian Blur" : 
         sigma = st.slider("Sigma = " , 0.0 , 5.0 , 1.0 , 0.5)
@@ -26,6 +27,12 @@ if uploaded_file is not None:
             result = gaussian_blur_filter(img, kernel_size, sigma)
         elif selected_filter == "Sharpen (Laplacian)":
             result = laplacian_filter(img)
+        elif selected_filter == "Sobel Edge Detection":
+            result = sobel_edge_detection(img)
+        elif selected_filter == "Prewitt Edge Detection":
+            result = prewitt_edge_detection(img)
+        
+        
 
         col1, col2 = st.columns(2)
         with col1:
